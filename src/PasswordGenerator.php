c6e63c04d0edb46fcd73a0d996ad4ea82a26af58
Carole Carduner <carole.carduner@gmail.com>
2014-11-20 12:12:31 +0100

    modify PasswordGenerator

--- src/PasswordGenerator.php
+++ src/PasswordGenerator.php
@@ -60,7 +60,7 @@ class PasswordGenerator
                 ?self::$passwordDefaultLength
                 : (int)$number;
 
-        $password = '';
+        $password = $char = '';
 
         switch($strength){
             case self::PASSWORD_EASY :
