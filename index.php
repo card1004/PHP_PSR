6346dd6a0956a09ed7226133bd40f6a672e6ee44
Carole Carduner <carole.carduner@gmail.com>
2014-11-20 13:01:08 +0100

    modification index

--- index.php
+++ index.php
@@ -8,9 +8,5 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-/*$passwordGenerator = new \Web1\StringGenerator\PasswordGenerator();
-
-echo \Web1\StringGenerator\PasswordGenerator::getRandomString(25, \Web1\StringGenerator\PasswordGenerator::PASSWORD_HARD);
-*/
 PDO::FETCH_OBJ;
 echo \Web1\StringGenerator\PasswordGenerator::getRandomString(50, \Web1\StringGenerator\PasswordGenerator::PASSWORD_HARD);
